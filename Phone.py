302809001a4caeff217d3f5e367033ac501a29d1
Ahmed Yasser El Sharkawy <166135885+Ahmed-Yasser-El-Sharkawy@users.noreply.github.com>
2024-12-22 19:17:17 +0200

    Update Phone.py

--- Phone.py
+++ Phone.py
@@ -20,21 +20,21 @@ def F_Mobile_Category( l ):
 
 # User Input
 def user_Input():
-    Ram = st.number_input('Enter RAM in Gigabytes',min_value=256,max_value=3998,step=1)
-    battery_power = st.number_input('Enter Battery Power in mAh',min_value=501,max_value=1998,step=1)
-    px_width = st.number_input('Enter Pixel Width in DPI',min_value=500,max_value=1960,step=1)
-    px_height = st.number_input('Enter Pixel Height in DPI',min_value=0,max_value=1998,step=1)
-    int_memory = st.number_input('Enter Internal Memory in Gigabytes',min_value=2,max_value=64,step=1)
-    sc_w = st.number_input('Enter Screen Width in Inches',min_value=0,max_value=18,step=1)
-    pc = st.number_input('Primary Camera mega pixels',min_value=0,max_value=20,step=1)
+    Ram = st.number_input('Enter RAM in Migabytes',min_value=256,max_value=9000,step=1)
+    battery_power = st.number_input('Enter Battery Power in mAh',min_value=501,max_value=4000,step=1)
+    px_width = st.number_input('Enter Pixel Width in DPI',min_value=500,max_value=4000,step=1)
+    px_height = st.number_input('Enter Pixel Height in DPI',min_value=0,max_value=4000,step=1)
+    int_memory = st.number_input('Enter Internal Memory in Gigabytes',min_value=2,max_value=512,step=1)
+    sc_w = st.number_input('Enter Screen Width in Inches',min_value=0,max_value=40,step=1)
+    pc = st.number_input('Primary Camera mega pixels',min_value=0,max_value=40,step=1)
     touch_screen = st.selectbox('Touch Screen', ['Yes', 'No'])
     if touch_screen == 'Yes':
-        touch_scr=1;
+        touch_scr=1
     else:
-        touch_scr=0;
-    mobile_wt = st.number_input('Enter Mobile Weight in Kg',min_value=80,max_value=200,step=1)
+        touch_scr=0
+    mobile_wt = st.number_input('Enter Mobile Weight in Kg',min_value=80,max_value=400,step=1)
     l =[Ram , battery_power, px_width , px_height,  int_memory, sc_w ,pc ,touch_scr, mobile_wt] 
-    return l;
+    return l
 
 # Display the user input
 l=user_Input()
